dd5b2de6b418b1f753cdd7b0ad2e59b15d77d187
eugenevorld <eugene_xhuang@hotmail.com>
2019-12-13 13:58:26 +0800

    Add ImageAcquirer base class

--- opendrop/app/core/imageacquisition/acquirers/__init__.py
+++ opendrop/app/core/imageacquisition/acquirers/__init__.py
@@ -1,6 +1,6 @@
 from injector import Module, Binder
 
-from .base import ImageAcquirerProvider
+from .base import ImageAcquirerProvider, ImageAcquirer
 from .filesystem import FilesystemAcquirerModule, FilesystemAcquirerProvider, FilesystemAcquirer
 from .usbcamera import USBCameraAcquirerModule, USBCameraAcquirerProvider, USBCameraAcquirer
 
--- opendrop/app/core/imageacquisition/acquirers/base.py
+++ opendrop/app/core/imageacquisition/acquirers/base.py
@@ -1,6 +1,12 @@
 from typing import Generic, TypeVar
 
-T = TypeVar('T')
+
+class ImageAcquirer:
+    def destroy(self) -> None:
+        """Destroy this object, releasing any resources no longer required."""
+
+
+T = TypeVar('T', bound=ImageAcquirer)
 
 
 class ImageAcquirerProvider(Generic[T]):
