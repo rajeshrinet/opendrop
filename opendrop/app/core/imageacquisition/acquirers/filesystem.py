1709b46635cba10ec5ee5adab9250db6c495982b
eugenevorld <eugene_xhuang@hotmail.com>
2019-12-13 14:53:51 +0800

    Implement a basic working version of FileSystemAcquirer and friends

--- opendrop/app/core/imageacquisition/acquirers/filesystem.py
+++ opendrop/app/core/imageacquisition/acquirers/filesystem.py
@@ -1,15 +1,43 @@
+import math
+from pathlib import Path
+from typing import Sequence
+
+import cv2
 from injector import Module, provider
 
+from opendrop.utility.bindable import BoxBindable, Bindable
 from .base import ImageAcquirerProvider
 
 
 class FilesystemAcquirer:
-    pass
+    def __init__(self, image_paths: Sequence[Path], frame_interval: float) -> None:
+        if not math.isfinite(frame_interval):
+            raise ValueError("Expected 'frame_interval' to be finite, got '{}'".format(frame_interval))
+
+        if len(image_paths) == 0:
+            raise ValueError("'image_paths' cannot be an empty sequence")
+
+        self.images = []
+        for image_path in image_paths:
+            image = cv2.imread(str(image_path), flags=cv2.IMREAD_COLOR)
+            self.images.append(image)
+
+        self.frame_interval = frame_interval
 
 
 class FilesystemAcquirerProvider(ImageAcquirerProvider[FilesystemAcquirer]):
+    def __init__(self) -> None:
+        self.image_paths = BoxBindable(initial=())  # type: Bindable[Sequence[Path]]
+        self.frame_interval = BoxBindable(initial=math.nan)  # type: Bindable[float]
+
     def get(self) -> FilesystemAcquirer:
-        return FilesystemAcquirer()
+        image_paths = self.image_paths.get()
+        frame_interval = self.frame_interval.get()
+
+        return FilesystemAcquirer(
+            image_paths=image_paths,
+            frame_interval=frame_interval,
+        )
 
 
 class FilesystemAcquirerModule(Module):
