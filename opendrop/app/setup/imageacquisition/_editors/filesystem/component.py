4670d27baf7e95cb55e0149c2d3eea5117d026e1
eugenevorld <eugene_xhuang@hotmail.com>
2019-12-14 12:53:09 +0800

    Bind selected images to model

--- opendrop/app/setup/imageacquisition/_editors/filesystem/component.py
+++ opendrop/app/setup/imageacquisition/_editors/filesystem/component.py
@@ -1,8 +1,12 @@
+import math
+from typing import Optional, MutableSequence, Callable
+
 from gi.repository import Gtk
 from injector import inject
 
 from opendrop.app.core.imageacquisition.acquirers import FilesystemAcquirerProvider
 from opendrop.appfw import WidgetComponent, WidgetView, Presenter
+from opendrop.utility.bindablegext import GObjectPropertyBindable
 from opendrop.widgets.file_chooser_button import FileChooserButton
 from opendrop.widgets.float_entry import FloatEntry
 
@@ -24,12 +28,12 @@ class FilesystemEditorView(WidgetView):
         file_chooser_lbl = Gtk.Label('Image files:', xalign=0)
         body.attach(file_chooser_lbl, 0, 0, 1, 1)
 
-        self._file_chooser_inp = FileChooserButton(
-            select_multiple=True
+        file_chooser_btn = FileChooserButton(
             filter=self._FILE_INPUT_FILTER,
+            select_multiple=True,
         )
-        self._file_chooser_inp.get_style_context().add_class('small-pad')
-        body.attach_next_to(self._file_chooser_inp, file_chooser_lbl, Gtk.PositionType.RIGHT, 1, 1)
+        file_chooser_btn.get_style_context().add_class('small-pad')
+        body.attach_next_to(file_chooser_btn, file_chooser_lbl, Gtk.PositionType.RIGHT, 1, 1)
 
         frame_interval_lbl = Gtk.Label('Frame interval (s):')
         body.attach(frame_interval_lbl, 0, 1, 1, 1)
@@ -37,14 +41,44 @@ class FilesystemEditorView(WidgetView):
         frame_interval_inp_container = Gtk.Grid()
         body.attach_next_to(frame_interval_inp_container, frame_interval_lbl, Gtk.PositionType.RIGHT, 1, 1)
 
-        self._frame_interval_inp = FloatEntry(lower=0, width_chars=6)
-        self._frame_interval_inp.get_style_context().add_class('small-pad')
-        frame_interval_inp_container.add(self._frame_interval_inp)
+        frame_interval_entry = FloatEntry(default=math.nan, lower=0, width_chars=6)
+        frame_interval_entry.get_style_context().add_class('small-pad')
+        frame_interval_inp_container.add(frame_interval_entry)
+
+        self.image_paths = GObjectPropertyBindable(
+            g_obj=file_chooser_btn,
+            prop_name='file-paths',
+        )
+
+        self.frame_interval = GObjectPropertyBindable(
+            g_obj=frame_interval_entry,
+            prop_name='value',
+        )
 
         body.foreach(Gtk.Widget.show_all)
 
+
 @FilesystemEditorComponent.presenter
-class FilesystemEditorPresenter(Presenter):
+class FilesystemEditorPresenter(Presenter[FilesystemEditorView]):
     @inject
     def __init__(self, *, acquirer_provider: FilesystemAcquirerProvider) -> None:
         self._provider = acquirer_provider
+
+        self._view = None  # type: Optional[FilesystemEditorView]
+        self._before_view_destroy_cleanup_tasks = []  # type: MutableSequence[Callable]
+
+    def after_view_init(self, view: FilesystemEditorView) -> None:
+        self._view = view
+
+        bindings = [
+            self._provider.frame_interval.bind(view.frame_interval),
+            self._provider.image_paths.bind(view.image_paths),
+        ]
+
+        self._before_view_destroy_cleanup_tasks += (
+            binding.unbind for binding in bindings
+        )
+
+    def before_view_destroy(self) -> None:
+        for f in self._before_view_destroy_cleanup_tasks:
+            f()
