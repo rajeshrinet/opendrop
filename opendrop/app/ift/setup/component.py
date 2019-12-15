b36dd6859272bc8c089f303976b456343dc7a6fe
eugenevorld <eugene_xhuang@hotmail.com>
2019-12-15 22:24:24 +0800

    Show error as message dialog when error occurs during setup.

--- opendrop/app/ift/setup/component.py
+++ opendrop/app/ift/setup/component.py
@@ -1,4 +1,4 @@
-from typing import Optional, Callable
+from typing import Optional, Callable, Any
 
 from gi.repository import Gtk, Gdk
 from injector import inject
@@ -7,6 +7,7 @@ from opendrop.app.ift.core import SetupModule
 from opendrop.app.ift.core.setup_service import SetupService
 from opendrop.app.setup.imageacquisition.component import ImageAcquisitionConfiguratorComponent
 from opendrop.appfw import WidgetComponent, WidgetView, Presenter, ComponentFactory
+from opendrop.widgets.error_dialog import ErrorDialog
 
 
 class SetupComponent(WidgetComponent):
@@ -53,11 +54,24 @@ class SetupView(WidgetView):
 
         window.foreach(Gtk.Widget.show_all)
 
+        self._window = window
+
     def _hdl_window_delete_event(self, window: Gtk.Window, data: Gdk.Event) -> bool:
         self._presenter.hdl_window_close()
 
         # return True to prevent window from closing.
         return True
+
+    def show_error(self, error: Any) -> None:
+        error_dialog = ErrorDialog(
+            parent=self._window,
+            title='Error',
+            text=str(error),
+        )
+
+        error_dialog.connect('response', lambda *_: error_dialog.destroy())
+
+        error_dialog.show()
 
 
 @SetupComponent.presenter
@@ -70,11 +84,15 @@ class SetupPresenter(Presenter['SetupView']):
         self._on_cancel = on_cancel or (lambda: None)
         self._on_close = on_close or (lambda: None)
 
+        self._view = None  # type: Optional[SetupView]
+
+    def after_view_init(self, view: SetupView) -> None:
+        self._view = view
+
     def hdl_continue_btn_clicked(self) -> None:
         error = self._service.set_up()
         if error:
-            print('Error occurred when setting up.')
-            print(error)
+            self._view.show_error(error)
             return
 
         self._on_success()
