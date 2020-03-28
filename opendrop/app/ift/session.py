6d54f5ee850fd84f4b1ef0cfbc32dbf04e632458
eugenevorld <eugene_xhuang@hotmail.com>
2020-03-28 18:04:57 +1100

    Clarify that image_acquirer is not to be injected

--- opendrop/app/ift/session.py
+++ opendrop/app/ift/session.py
@@ -5,5 +5,5 @@ from opendrop.app.common.core.imageacquirer import ImageAcquirer
 
 class IFTSession:
     @inject
-    def __init__(self, image_acquirer: ImageAcquirer) -> None:
+    def __init__(self, *, image_acquirer: ImageAcquirer) -> None:
         self.image_acquirer = image_acquirer
