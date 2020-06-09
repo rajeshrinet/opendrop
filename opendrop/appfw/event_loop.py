c6dd833e1b2df2cc4034fe22da287a93fd46f780
Eugene Huang <eugene.huang99@gmail.com>
2020-06-09 13:40:25 +1000

    Refactor glibcoro stuff

--- opendrop/appfw/event_loop.py
+++ opendrop/appfw/event_loop.py
@@ -5,8 +5,8 @@ from injector import Module, Binder, InstanceProvider
 
 class _EventLoopModule(Module):
     def configure(self, binder: Binder) -> None:
-        from opendrop.vendor.glibcoro import glibcoro
-        glibcoro.install()
+        from opendrop.vendor.glibcoro import GLibEventLoopPolicy
+        asyncio.set_event_loop_policy(GLibEventLoopPolicy())
 
         binder.bind(
             interface=asyncio.AbstractEventLoop,
